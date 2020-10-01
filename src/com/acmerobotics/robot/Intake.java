fae7f1f6bab2ab63d87e026d195aa3a7c70cf7ba
Jesus Betancourt <jbeta02@gmail.com>
2020-10-01 16:41:19 -0700

    basic structure for Intake. Most importat is that I made the intake class as well as the subsystems highist level function so it can be used in other classes such as opModes

---
+++ src/com/acmerobotics/robot/Intake.java
@@ -0,0 +1,38 @@
+package com.acmerobotics.robot;
+
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Intake {
+
+
+    /**
+     * Create motor objects
+     *
+     * @param hardwareMap setup hardwareMap for hardware device
+     */
+    public Intake(HardwareMap hardwareMap){
+
+    }
+
+
+    /**
+     * Highest level method.
+     *
+     * Method will set motor powers to intake ring or rings.
+     */
+    public void intakeRings(){
+
+    }
+
+
+    /**
+     * Set the power of all intake motors
+     *
+     * @param power the power to be set
+     */
+    private void setPower(double power){
+
+    }
+
+}
