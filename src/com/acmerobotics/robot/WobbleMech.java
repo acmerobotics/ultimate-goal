204e100847271089a7a0c9945b81dc98b5614096
Jesus Betancourt <jbeta02@gmail.com>
2020-10-01 16:51:28 -0700

    basic wobbleMech structure. Highest levle methods added

---
+++ src/com/acmerobotics/robot/WobbleMech.java
@@ -0,0 +1,30 @@
+package com.acmerobotics.robot;
+
+public class WobbleMech {
+
+
+    public WobbleMech(){
+
+    }
+
+
+    /**
+     * Highest level method.
+     *
+     * Will place down wobble.
+     */
+    public void dropWobble(){
+
+    }
+
+
+    /**
+     * Highest level method.
+     *
+     * Will place down wobble.
+     */
+    public void pickUpWobble(){
+
+    }
+
+}
