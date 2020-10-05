ef3df9d8812fa3562e773c38b2582e66aa12a9dc
Eli Barbieri <elicbarbieri@gmail.com>
2020-10-04 17:43:31 -0700

    added launch timing and strucuture of testing opMode

--- src/com/acmerobotics/opmodes/LauncherPrototype.java
+++ src/com/acmerobotics/opmodes/LauncherPrototype.java
@@ -3,7 +3,8 @@ package com.acmerobotics.opmodes;
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
@@ -18,27 +19,76 @@ This will be used to test the time it takes from shooting to ready to shoot agai
 @Config
 public class LauncherPrototype extends LinearOpMode {
 
-    private double flywheelGearReduction;
+    private double motorGearReduction = 4.0;
+    private double LauncherGearReduction =1.0/6.0;
+    private double flywheelGearReduction = motorGearReduction * LauncherGearReduction;
+    public double ticksPerRevolution = 7 * flywheelGearReduction;
 
-    public static double motorPower; //dashboard configurable
-    public static double freeFlywheelSpeed; //dashboard configurable
-    public static double flywheelLaunchThreshold; //dashboard configurable
+    public static double motorPower = 1; //dashboard configurable
+    public static double motorTargetVelocityRPM = 0; //dashboard configurable
+    public static double freeFlywheelSpeed = 0; //dashboard configurable
+    public static double flywheelLaunchThreshold = 0; //dashboard configurable
+
+    public double currentAccelMotorVelocityRPM;
+    public double currentShooterMotorVelocityRPM;
+    public double averageMotorVelocityRPM;
+
+    private String flywheelStatus;
+    private double[] speedResetTimes = {};
 
     @Override
     public void runOpMode(){
         FtcDashboard dashboard = FtcDashboard.getInstance();
         Telemetry dashboardTelementry = dashboard.getTelemetry();
 
-        DcMotor launcherAccelMotor = hardwareMap.get(DcMotor.class,"accelMotor");
-        DcMotor launcherShooterMotor = hardwareMap.get(DcMotor.class,"shooterMotor");
-        launcherAccelMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER); //PID controlled velocity
-        launcherShooterMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER); //PID controlled velocity
+        ElapsedTime flywheelSpinTime = new ElapsedTime();
+
+        DcMotorEx launcherAccelMotor = hardwareMap.get(DcMotorEx.class,"accelMotor");
+        DcMotorEx launcherShooterMotor = hardwareMap.get(DcMotorEx.class,"shooterMotor");
+        launcherAccelMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER); //PID controlled velocity
+        launcherShooterMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER); //PID controlled velocity
 
         waitForStart();
 
         while (!isStopRequested()) {
+            //set motorPower to 1 in dashboard to test the max rmp of the launcher, then set motorPower to 0 and set rpms for velocity PID
+            if(motorPower != 0){
+                launcherAccelMotor.setPower(motorPower);
+                launcherShooterMotor.setPower(motorPower);
+            }
+            else {
+                launcherAccelMotor.setVelocity(motorTargetVelocityRPM * (ticksPerRevolution / 60));
+                launcherShooterMotor.setVelocity(motorTargetVelocityRPM * (ticksPerRevolution / 60));
+            }
 
+            currentAccelMotorVelocityRPM = launcherAccelMotor.getVelocity() * (60 / ticksPerRevolution);
+            currentShooterMotorVelocityRPM = launcherShooterMotor.getVelocity() * (60 / ticksPerRevolution);
+            averageMotorVelocityRPM = (currentAccelMotorVelocityRPM + currentShooterMotorVelocityRPM) /2;
+
+            if(Math.abs(freeFlywheelSpeed-averageMotorVelocityRPM) >= flywheelLaunchThreshold || flywheelStatus.equals("Speeding Up")) {
+                 flywheelSpinTime.reset();
+                 flywheelStatus = "Speeding Up";
+
+                 if(Math.abs(freeFlywheelSpeed-averageMotorVelocityRPM) <= flywheelLaunchThreshold) {
+                     flywheelStatus = "Ready to Launch";
+
+                     speedResetTimes[speedResetTimes.length] = flywheelSpinTime.milliseconds();
+                 }
+            }
+
+            dashboardTelementry.addData("currentAccelMotorSpeed: ", currentAccelMotorVelocityRPM);
+            dashboardTelementry.addData("currentShooterMotorSpeed: ", currentShooterMotorVelocityRPM);
+            dashboardTelementry.addData("Flywheel Status: ", flywheelStatus);
+            dashboardTelementry.update();
         }
+        //outputting Flywheel Reset Times
+        double averageResetTime = speedResetTimes[0];
+        for(int i=1; i < speedResetTimes.length; i++) {
+            averageResetTime = (averageResetTime + speedResetTimes[i]) / 2;
+        }
+
+        dashboardTelementry.addData("Average Shooter Reset Time: ", averageResetTime);
+        dashboardTelementry.update();
     }
 
 
