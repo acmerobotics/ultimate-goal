0c852e865d646ef253a82ffd89bbed31367f3b49
Jesus Betancourt <jbeta02@gmail.com>
2020-10-13 23:03:37 -0700

    added park to shoot autos

--- src/com/acmerobotics/opmodes/auto/Shoot.java
+++ src/com/acmerobotics/opmodes/auto/Shoot.java
@@ -10,59 +10,75 @@ public class Shoot extends Auto {
     public void runBlueA(){
         ACMERobot robot = new ACMERobot(this);
 
-        robot.drive.moveForward(80 - (robot.len / 2));
+        robot.drive.moveForward(80 - robot.len);
         robot.runUntil(robot.drive::atYPosition);
 
         robot.drive.strafeRight(12);
         robot.runUntil(robot.drive::atStrafePosition);
 
         robot.drive.stopMotors();
+        robot.update();
 
         // shoot rings
+
+        robot.drive.moveForward(robot.len / 2);
+        robot.runUntil(robot.drive::atYPosition);
     }
 
     @Override
     public void runBlueB(){
         ACMERobot robot = new ACMERobot(this);
 
-        robot.drive.moveForward(80 - (robot.len / 2));
+        robot.drive.moveForward(80 - robot.len);
         robot.runUntil(robot.drive::atYPosition);
 
         robot.drive.strafeLeft(12);
         robot.runUntil(robot.drive::atStrafePosition);
 
         robot.drive.stopMotors();
+        robot.update();
 
         // shoot rings
+
+        robot.drive.moveForward(robot.len / 2);
+        robot.runUntil(robot.drive::atYPosition);
     }
 
     @Override
     public void runRedB(){
         ACMERobot robot = new ACMERobot(this);
 
-        robot.drive.moveForward(80 - (robot.len / 2));
+        robot.drive.moveForward(80 -robot.len);
         robot.runUntil(robot.drive::atYPosition);
 
         robot.drive.strafeRight(12);
         robot.runUntil(robot.drive::atStrafePosition);
 
         robot.drive.stopMotors();
+        robot.update();
 
         // shoot rings
+
+        robot.drive.moveForward(robot.len / 2);
+        robot.runUntil(robot.drive::atYPosition);
     }
 
     @Override
     public void runRedA(){
         ACMERobot robot = new ACMERobot(this);
 
-        robot.drive.moveForward(80 - (robot.len / 2));
+        robot.drive.moveForward(80 - robot.len);
         robot.runUntil(robot.drive::atYPosition);
 
         robot.drive.strafeLeft(12);
         robot.runUntil(robot.drive::atStrafePosition);
 
         robot.drive.stopMotors();
+        robot.update();
 
         // shoot rings
+
+        robot.drive.moveForward(robot.len / 2);
+        robot.runUntil(robot.drive::atYPosition);
     }
 }
