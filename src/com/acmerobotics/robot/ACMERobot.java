c7d1515a432d48577d2bcb3c8931f371248cbfb4
Jesus Betancourt <jbeta02@gmail.com>
2020-11-06 22:09:51 -0800

    created intake, added intake to Robot, and modified TeleOp.

--- src/com/acmerobotics/robot/ACMERobot.java
+++ src/com/acmerobotics/robot/ACMERobot.java
@@ -6,13 +6,18 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 public class ACMERobot extends Robot {
 
     public final Drive drive;
+    public final Intake intake;
 
     public ACMERobot(LinearOpMode opMode){
         super(opMode);
 
+        //registerHub("Expansion Hub 1");
         registerHub("Expansion Hub 2");
 
         drive = new Drive(this, opMode);
+        intake = new Intake(this);
+
         registerSubsytem(drive);
+        registerSubsytem(intake);
     }
 }
--- src/com/acmerobotics/robot/Intake.java
+++ src/com/acmerobotics/robot/Intake.java
@@ -1,38 +1,75 @@
 package com.acmerobotics.robot;
 
+import com.acmerobotics.dashboard.canvas.Canvas;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.robomatic.hardware.CachingDcMotorEx;
+import com.acmerobotics.robomatic.robot.Robot;
+import com.acmerobotics.robomatic.robot.Subsystem;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
-public class Intake {
+@Config
+public class Intake extends Subsystem {
+
+    private DcMotorEx intakeMotor;
+    private Servo intakeServo;
+
+    public static double upPosition = 0;
+    public static double downPosition = 0;
 
 
-    /**
-     * Create motor objects
-     *
-     * @param hardwareMap setup hardwareMap for hardware device
-     */
-    public Intake(HardwareMap hardwareMap){
+    private int num = 0;
+
+    public Intake(Robot robot){
+        super("Intake");
+
+        intakeMotor = robot.getMotor("intakeMotor");
+        intakeServo = robot.getServo("intakeServo");
+    }
+
+    @Override
+    public void update(Canvas overlay){
+        telemetryData.addData("upPosition", upPosition);
+        telemetryData.addData("downPosition", downPosition);
+        telemetryData.addData("num", num);
 
     }
 
 
-    /**
-     * Highest level method.
-     *
-     * Method will set motor powers to intake ring or rings.
-     */
     public void intakeRings(){
 
+        boolean runIntake = isOdd();
+
+        if (runIntake){
+
+            dropIntake(true);
+            intakeMotor.setPower(1);
+        }
+        else{
+
+            dropIntake(false);
+            intakeMotor.setPower(0);
+        }
     }
 
 
-    /**
-     * Set the power of all intake motors
-     *
-     * @param power the power to be set
-     */
-    private void setPower(double power){
+    private void dropIntake(boolean lower){
+        if (lower){
+            intakeServo.setPosition(downPosition);
+        }
 
+        else{
+            intakeServo.setPosition(upPosition);
+        }
     }
 
+
+    private boolean isOdd(){
+        num++;
+
+        return  !(num % 2 == 0);
+    }
 }
