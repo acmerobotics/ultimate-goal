48d0958e8b8a7c584df98900ac8d687cc57e3672
Emma Sheffo <emma_otter@icloud.com>
2021-03-07 19:28:34 -0800

    committing to reopen android studio

---
+++ src/com/acmerobotics/opmodes/auto/powershot/PowerShotBlueA.java
@@ -0,0 +1,83 @@
+package com.acmerobotics.opmodes.auto.powershot;
+
+import com.acmerobotics.opmodes.auto.Auto;
+import com.acmerobotics.robot.ACMERobot;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(group = "power shot")
+public class PowerShotBlueA extends Auto {
+
+    @Override
+    public void run() throws InterruptedException {
+        ACMERobot robot = new ACMERobot(this);
+
+        robot.intake.moveServo();
+        robot.update();
+
+        //move into positon
+        robot.drive.moveForward(80 - (robot.len + robot.errorMargin));
+        robot.runUntil(robot.drive::atYPosition);
+
+        robot.drive.turnTo(0);
+        robot.runForTime(2000);
+
+        robot.drive.strafeRight(16); //haha this probably isn't right
+        robot.runUntil(robot.drive::atStrafePosition);
+
+        robot.drive.moveBack(4);
+        robot.runUntil(robot.drive::atYPosition);
+
+        robot.drive.stopMotors();
+        robot.update();
+
+        //shoot first ring
+        robot.launcher.shootHigh();
+        robot.runUntil(robot.launcher::isMaxVelocity);
+        robot.shootRingA();
+
+        //move to next power shot
+        robot.drive.turnTo(0);
+        robot.runForTime(2000);
+
+        robot.drive.strafeRight(7.5);
+        robot.runUntil(robot.drive::atStrafePosition);
+
+        robot.drive.moveBack(4);
+        robot.runUntil(robot.drive::atYPosition);
+
+        robot.drive.stopMotors();
+        robot.update();
+
+        //shoot second ring
+        robot.launcher.shootHigh();
+        robot.runUntil(robot.launcher::isMaxVelocity);
+        robot.shootRingA();
+
+        //move to final power shot
+        robot.drive.turnTo(0);
+        robot.runForTime(2000);
+
+        robot.drive.strafeRight(7.5);
+        robot.runUntil(robot.drive::atStrafePosition);
+
+        robot.drive.moveBack(4);
+        robot.runUntil(robot.drive::atYPosition);
+
+        robot.drive.stopMotors();
+        robot.update();
+
+        //shoot final ring
+        robot.launcher.shootHigh();
+        robot.runUntil(robot.launcher::isMaxVelocity);
+        robot.shootRingA();
+
+        //park
+        robot.drive.moveForward((robot.len /2) + 2);
+        robot.runUntil(robot.drive::atYPosition);
+
+        robot.drive.stopMotors();
+        robot.runUntilStop();
+
+
+    }
+}
